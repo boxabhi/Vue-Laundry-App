3408289fc3555bf678f6a7127d8036f4f1af14b1
abhijeet gupta <abhijeetg40@gmail.com>
2020-06-08 21:14:45 +0530

    -

--- src/components/utils/Navbar.vue
+++ src/components/utils/Navbar.vue
@@ -1,45 +1,45 @@
 <template>
 
   <v-bottom-navigation  style="padding-bottom:20px;padding-top:5px" class="p-5 mt-4" fixed background-color="primary" color="#fff">
-    <v-btn class="mr-5">
-      <router-link to="/">
-       <v-btn class="font-weight-bold"  style="color:#fff !important">
+    
+      <router-link to="/" class="noHover">
+       <v-btn class="font-weight-bold noHover"  style="color:#fff !important">
         Home
-        <v-icon color="#fff" medium>mdi-home</v-icon>
-       </v-btn>
+        <v-icon color="#fff"  medium>mdi-home</v-icon>
+        </v-btn>
       </router-link>
 
-    </v-btn>
+   
 
-    <v-btn>
-      <router-link to="/cart">
-      <v-btn class="font-weight-bold"  style="color:#fff !important">
+    
+      <router-link to="/cart" class="noHover">
+      <v-btn class="font-weight-bold noHover"  style="color:#fff !important">
         Order
-        <v-icon color="#fff" medium>mdi-cart</v-icon>
+        <v-icon color="#fff" class="noHover" medium>mdi-cart</v-icon>
       </v-btn>
       </router-link>
-    </v-btn>
+    
 
-    <v-btn>
+    
       <router-link to="/offer">
        <v-btn class="font-weight-bold"  style="color:#fff !important">
         Offers
         <v-icon color="#fff" medium>mdi-label</v-icon>
        </v-btn>
       </router-link>
-    </v-btn>
+    
 
-     <v-btn>
+    
       <router-link to="/membership">
        <v-btn class="font-weight-bold"  style="color:#fff !important">
         My Plan
         <v-icon color="#fff" medium>mdi-wallet-membership</v-icon>
        </v-btn>
       </router-link>
-    </v-btn>
+   
 
 
-    <v-btn>
+   
       <router-link to="/login">
  <v-btn class="font-weight-bold"  style="color:#fff !important">
         Account
@@ -47,19 +47,28 @@
  </v-btn>
       </router-link>
 
-    </v-btn>
+    
   </v-bottom-navigation>
 </template>
 
 
 <style scoped>
+.noHover{
+    background: #1976d2 !important;
+}
+.noHover :hover{
+    background: #1976d2 !important;
+}
 router-link{
+  background :none !important;
+}
+router-link:hover{
   background :none !important;
 }
 v-btn {
   background : none !important;
 }
 v-btn :hover {
-  background : none !important;
+  background : #1976d2 !important;
 }
 </style>
